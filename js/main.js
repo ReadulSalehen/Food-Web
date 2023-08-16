92a27eb3bb76b17760ac5d041caeec28e34ae3ec
Readul Salehen <102899309+ReadulSalehen@users.noreply.github.com>
2023-08-17 00:17:30 +0600

    Add files via upload
    initial commit

---
+++ js/main.js
@@ -0,0 +1,43 @@
+
+
+// active navbar
+let nav = document.querySelector(".navigation-wrap");
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 20) {
+        nav.classList.add("scroll-on");
+    } else {
+        nav.classList.remove("scroll-on");
+    }
+}
+
+// nav hide
+let navBar = document.querySelectorAll('.nav-link');
+let navCollapse = document.querySelector('.navbar-collapse.collapse');
+navBar.forEach(function (a) {
+    a.addEventListener("click", function () {
+        navCollapse.classList.remove("show");
+    })
+})
+
+//counter design
+document.addEventListener("DOMContentLoaded", () => {
+    function counter(id, start, end, duration) {
+        let obj = document.getElementById(id),
+            current = start,
+            rang = end - start,
+            increment = end > start ? 1 : -1,
+            step = Math.abs(Math.floor(duration / rang)),
+            timer = setInterval(() => {
+                current += increment;
+                obj.textContent = current;
+                if (current == end) {
+                    clearInterval(timer);
+                }
+            }, step);
+    }
+    counter("count1", 0, 125, 3000);
+    counter("count2", 50, 3125, 1213);
+    counter("count3", 0, 2135, 3000);
+    counter("count4", 0, 7564, 3000);
+
+});
